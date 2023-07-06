e596777c3bfa9ad19b543937ce72506d356b9a3c
Loc Nguyen <tanlocit20062000@gmail.com>
2023-07-06 11:57:10 +0700

    [update] end of section 4

--- backend/API/Controllers/AuthController.cs
+++ backend/API/Controllers/AuthController.cs
@@ -5,19 +5,22 @@ using System.Security.Cryptography;
 using System.Text;
 using Microsoft.EntityFrameworkCore;
 using API.DTOs;
+using API.Interfaces;
 
 namespace API.Controllers
 {
   public class AuthController : BaseApiController
   {
     private readonly DataContext _context;
-    public AuthController(DataContext context)
+    private readonly ITokenService _tokenService;
+    public AuthController(DataContext context, ITokenService tokenService)
     {
+      _tokenService = tokenService;
       _context = context;
     }
 
     [HttpPost("register")] // POST: api/auth/register
-    public async Task<ActionResult<AppUser>> Register(RegisterDTO registerDTO)
+    public async Task<ActionResult<UserDTO>> Register(RegisterDTO registerDTO)
     {
       if (await UserExists(registerDTO.UserName)) return BadRequest("UserName is taken");
 
@@ -30,11 +33,15 @@ namespace API.Controllers
       };
       _context.Users.Add(user);
       await _context.SaveChangesAsync();
-      return user;
+      return new UserDTO
+      {
+        UserName = user.UserName,
+        // Token = _tokenService.CreateToken(user)
+      };
     }
 
     [HttpPost("login")]
-    public async Task<ActionResult<AppUser>> Login(LoginDTO loginDTO)
+    public async Task<ActionResult<UserDTO>> Login(LoginDTO loginDTO)
     {
       var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName == loginDTO.UserName);
       if (user == null) return Unauthorized();
@@ -44,7 +51,11 @@ namespace API.Controllers
       {
         if (computedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid Password");
       }
-      return user;
+      return new UserDTO
+      {
+        UserName = user.UserName,
+        Token = _tokenService.CreateToken(user)
+      };
     }
     private async Task<bool> UserExists(string username)
     {
